9a0e893fd88c1bc9c8d035a993fce2bff8c9266f
aaug <aaug@caasco.ca>
2022-03-10 15:52:26 -0500

    displaying card

--- fruitsAndVeggies.js
+++ fruitsAndVeggies.js
@@ -10,29 +10,35 @@ xmlhttp.onreadystatechange = function () {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function myFunction(arr) {
+function myFunction(personalDetails) {
    var out = "";
    var i;
-   for (i = 0; i < arr.length; i++) {
-      out += `<div class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-         <img src="${arr[i].avatar}" />
+   for (i = 0; i < personalDetails.length; i++) {
+      out += `<div class="">
+         <img src="${personalDetails[i].avatar}" />
          <div class="flex flex-col">
-            <h2>${arr[i].first_name} ${arr[i].last_name}</h2>
+            <h2>${personalDetails[i].first_name} ${personalDetails[i].last_name}</h2>
+            <span><strong>ID:</strong> 33${personalDetails[i].id}78</span>
 
-            <span><strong>ID:</strong> ${arr[i].id}</span>
-            <span><strong>Address:</strong> ${arr[i].address}</span>
-            <span><strong>City:</strong> ${arr[i].city}</span>
-            <span><strong>Province:</strong> ${arr[i].province}</span>
-            <span><strong>CountryCode:</strong> ${arr[i].countryCode}</span>
-            <span><strong>Email:</strong> ${arr[i].email}</span>
-            <span><strong>Gender:</strong> ${arr[i].gender}</span>
-            <span><strong>Race:</strong> ${arr[i].race}</span>
-            <span><strong>BTC:</strong> ${arr[i].btc}</span>
-            <span><strong>Paragraph:</strong> ${arr[i].paragraph}</span>
-            <span><strong>Company Name:</strong> ${arr[i].companyName}</span>
-            <span><strong>Slogan:</strong> ${arr[i].slogan}</span>
-            <span><strong>Latitude:</strong> ${arr[i].latitude}</span>
-            <span><strong>Longitude:</strong> ${arr[i].longitude}</span>
+            <h3>Address:</h3>
+            <span>${personalDetails[i].address}</span>
+            <span>${personalDetails[i].city}, ${personalDetails[i].province ? `${personalDetails[i].province},` : ''} ${personalDetails[i].countryCode}</span>
+
+            ${personalDetails[i].btc ? `
+            <h3>Crypto</h3>
+            <h4>BTC Address:</h4>
+               <a class="text-blue-500" href="https://www.blockchain.com/btc/address/${personalDetails[i].btc}" target="_blank">
+                  ${personalDetails[i].btc}
+               </a>` : ''}
+
+            <h3>Last Known Location</h3>
+            <span><a class="text-blue-500" href="https://www.google.com/maps/place/${personalDetails[i].latitude},%20${personalDetails[i].longitude}" target="_blank">${personalDetails[i].latitude}, ${personalDetails[i].longitude}</a></span>
+
+            <span><strong>Gender:</strong> ${personalDetails[i].gender} <strong>Race:</strong> ${personalDetails[i].race}</span>
+            ${personalDetails[i].paragraph ? `<span><strong>Quote:</strong> ${personalDetails[i].paragraph}</span>` : ''}
+            <h3>Company Info</h3>
+            <h4>${personalDetails[i].companyName}</h4>
+            <span>${personalDetails[i].slogan}</span>
          </div>
       </div>`;
    }
