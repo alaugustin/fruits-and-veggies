e7bcfb16ae9f19441fc4e110d7f80314bf08214f
aaug <aaug@caasco.ca>
2022-03-11 14:19:09 -0500

    UX update

--- fruitsAndVeggies.js
+++ fruitsAndVeggies.js
@@ -3,27 +3,49 @@ const listHolder = document.getElementById("listHolder");
 
 personalDetails.forEach(function (value, i) {
    html +=
-      `<div class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-         <img src="${personalDetails[i].avatar}" />
+      `<article id="${personalDetails[i].id}" class="border border-gray-400 rounded-lg p-4">
+         <img class="mb-1"src="${personalDetails[i].avatar}" />
          <div class="flex flex-col">
-            <h2>${personalDetails[i].first_name} ${personalDetails[i].last_name}</h2>
+            <div class="mb-4">
+               <h2 class="text-3xl mb-1">
+                  <strong>ID:</strong> ${personalDetails[i].ein}
+               </h2>
+               <h3 class="text-2xl mb-2"><strong>Firmware Version: </strong>${personalDetails[i].version}</h3>
 
-            <span><strong>ID:</strong> ${personalDetails[i].id}</span>
-            <span><strong>Address:</strong> ${personalDetails[i].address}</span>
-            <span><strong>City:</strong> ${personalDetails[i].city}</span>
-            <span><strong>Province:</strong> ${personalDetails[i].province}</span>
-            <span><strong>CountryCode:</strong> ${personalDetails[i].countryCode}</span>
-            <span><strong>Email:</strong> ${personalDetails[i].email}</span>
-            <span><strong>Gender:</strong> ${personalDetails[i].gender}</span>
-            <span><strong>Race:</strong> ${personalDetails[i].race}</span>
-            <span><strong>BTC:</strong> ${personalDetails[i].btc}</span>
-            <span><strong>Paragraph:</strong> ${personalDetails[i].paragraph}</span>
-            <span><strong>Company Name:</strong> ${personalDetails[i].companyName}</span>
-            <span><strong>Slogan:</strong> ${personalDetails[i].slogan}</span>
-            <span><strong>Latitude:</strong> ${personalDetails[i].latitude}</span>
-            <span><strong>Longitude:</strong> ${personalDetails[i].longitude}</span>
+               <div class="flex mb-2">
+                  <p class="flex flex-col basis-1/3"><strong>Registered to:</strong> ${personalDetails[i].first_name} ${personalDetails[i].last_name}</p>
+                  <p class="flex flex-col basis-1/3"><strong>Gender:</strong> ${personalDetails[i].gender}</p>
+                  <p class="flex flex-col basis-1/3"><strong>Race:</strong> ${personalDetails[i].race}</p>
+               </div>
+
+               ${personalDetails[i].paragraph ? `<p class="border p-2">“${personalDetails[i].paragraph}”</p>` : ''}
+            </div>
+
+            <div class="mb-4">
+               <h3 class="text-2xl mb-2">Last Known Location</h3>
+               <a class="text-blue-500" href="https://www.google.com/maps/place/${personalDetails[i].latitude},%20${personalDetails[i].longitude}" target="_blank">${personalDetails[i].latitude}, ${personalDetails[i].longitude}</a>
+               <p>${personalDetails[i].date} ${personalDetails[i].time}</p>
+            </div>
+
+            <div class="mb-4">
+               <h3 class="text-2xl mb-2">Manufacturer Info:</h3>
+               <h4 class="text-xl mb-2">${personalDetails[i].companyName}</h4>
+               <p>${personalDetails[i].address}</p>
+               <p>${personalDetails[i].city}, ${personalDetails[i].province ? `${personalDetails[i].province},` : ''} ${personalDetails[i].countryCode}</p>
+               <p class="italic">${personalDetails[i].slogan.charAt(0).toUpperCase() + personalDetails[i].slogan.slice(1)}</p>
+            </div>
+
+            <div class="mb-4">
+               ${personalDetails[i].btc || personalDetails[i].eth ? `<h3 class="text-2xl mb-2">Blockchain</h3>` : ''}
+
+               ${personalDetails[i].btc ? `<h4 class="text-xl mb-2">BTC Address:</h4>
+               <a class="text-blue-500" href="https://www.blockchain.com/btc/address/${personalDetails[i].btc}" target="_blank">${personalDetails[i].btc}</a>` : ''}
+
+               ${personalDetails[i].eth ? `<h4 class="text-xl mb-2">ETC Address:</h4>
+               <a class="text-blue-500" href="https://etherscan.io/address/${personalDetails[i].eth}" target="_blank">${personalDetails[i].eth}</a>` : ''}
+            </div>
          </div>
-      </div>`;
+      </article>`;
 });
 
 document.getElementById("listHolder").innerHTML += html;
